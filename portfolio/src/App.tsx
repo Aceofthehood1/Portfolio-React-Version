4d155e6f2142d65b9e4a8aac42c52efd3facce2c
Aceofthehood1 <geoffreyameble81@gmail.com>
2025-10-04 00:04:08 +0100

    Added tailwind, node and daisyUI packages to application

---
+++ portfolio/src/App.tsx
@@ -0,0 +1,18 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome' 
+import { faHouse } from "@fortawesome/free-solid-svg-icons" 
+
+function App() {
+
+  return (
+    <>
+      <div className="flex flex-col items-center justify-center h-screen bg-amber-500">
+        <h2>Hi, I'm</h2> 
+        <h1>GEOFFREY AMEBLE</h1>
+        <hr></hr>
+        <h2>Web Developer and Tech Analyst</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
