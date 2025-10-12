76c139d5612ed95661576c81c45cee6db2f40315
Aceofthehood1 <geoffreyameble81@gmail.com>
2025-10-12 20:00:03 +0100

    Designed homepage with relevant content and included accordion in portfolio page with experiences and projects section.

--- portfolio/src/Pages/HomePage.tsx
+++ portfolio/src/Pages/HomePage.tsx
@@ -1,17 +1,49 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Link } from "react-router-dom";
+import { faFileLines } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 interface Props {}
 
 const HomePage = (props: Props) => {
   return (
     <>
-    <div className="flex flex-col items-center justify-center h-screen bg-amber-500">
-      <h2>Hi, I'm</h2>
-      <h1>GEOFFREY AMEBLE</h1>
-      <hr></hr>
-      <h2>Web Developer and Software Engineer</h2>
-      <Link to="/portfolio">Sup bro</Link>
-    </div>
+      <div className="flex flex-col items-center justify-center h-screen bg-amber-500">
+        <div className="text-left max-w-[800px]">
+          <h2 className="text-5xl mb-2">Hi, I'm</h2>
+          <h1 className="text-5xl">GEOFFREY AMEBLE</h1>
+          <hr className="border-2"></hr>
+          <h2 className="text-3xl mb-2">Web Developer and Software Engineer</h2>
+          <div className="flex flex-row gap-2 text-lg">
+            <button className="bg-blue-700 border-2 border-black rounded-lg text-white p-2">
+              <Link to="/portfolio">
+                <FontAwesomeIcon icon={faFileLines} />
+                Portfolio
+              </Link>
+            </button>
+            <button className="bg-blue-700 border-2 border-black rounded-lg text-white p-2">
+              <a
+                href="https://github.com/Aceofthehood1"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={faGithub} />
+                Github
+              </a>
+            </button>
+            <button className="bg-blue-700 border-2 border-black rounded-lg text-white p-2">
+              <a
+                href="https://www.linkedin.com/in/geoffrey-ameble/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={faFileLines} />
+                LinkedIn
+              </a>
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- portfolio/src/Pages/Portfolio.tsx
+++ portfolio/src/Pages/Portfolio.tsx
@@ -3,10 +3,85 @@ interface Props {}
 const Portfolio = (props: Props) => {
   return (
     <>
-          <h1>Hello Mate</h1>
-      </>
-  )
-}
+      <div className="flex flex-col gap-8 sm:flex-row items-center justify-center mt-[100px]">
+        <div className=" flex flex-col gap-2 max-w-[600px]">
+          <h1 className="text-5xl mb-4">Experiences</h1>
+          <div className="collapse collapse-plus bg-base-100 border border-base-300">
+            <input type="radio" name="my-accordion-3" defaultChecked />
+            <div className="collapse-title font-semibold flex justify-between">
+              <span>SAP Investment Holdings</span>
+              <span>Feb-2024 - Current</span>
+            </div>
+            <div className="collapse-content text-sm">
+              Click the "Sign Up" button in the top right corner and follow the
+              registration process.
+            </div>
+          </div>
+          <div className="collapse collapse-plus bg-base-100 border border-base-300">
+            <input type="radio" name="my-accordion-3" />
+            <div className="collapse-title font-semibold flex justify-between">
+              <span>Kato</span>
+              <span>March-2025 - July-2025</span>
+            </div>
+            <div className="collapse-content text-sm">
+              Click on "Forgot Password" on the login page and follow the
+              instructions sent to your email.
+            </div>
+          </div>
+          <div className="collapse collapse-plus bg-base-100 border border-base-300">
+            <input type="radio" name="my-accordion-3" />
+            <div className="collapse-title font-semibold flex justify-between">
+              <span>Turntabl</span>
+              <span>June-2022 - August-2022</span>
+            </div>
+            <div className="collapse-content text-sm">
+              Go to "My Account" settings and select "Edit Profile" to make
+              changes.
+            </div>
+          </div>
+        </div>
 
-export default Portfolio
+        <div className="flex flex-col max-w-[600px]">
+          <h1 className="text-5xl mb-4">Projects</h1>
+          <div className=" flex flex-col gap-2 max-w-[600px]">
+            <div className="collapse collapse-plus bg-base-100 border border-base-300">
+              <input type="radio" name="my-accordion-2" defaultChecked />
+              <div className="collapse-title font-semibold flex justify-between">
+              <span>Restaurant Reservation Marketplace</span>
+              <span>June-2022 - August-2022</span>
+            </div>
+              <div className="collapse-content text-sm">
+                Click the "Sign Up" button in the top right corner and follow
+                the registration process.
+              </div>
+            </div>
+            <div className="collapse collapse-plus bg-base-100 border border-base-300">
+              <input type="radio" name="my-accordion-2" />
+              <div className="collapse-title font-semibold flex justify-between">
+              <span>Vehicle Management System</span>
+              <span>June-2022 - August-2022</span>
+            </div>
+              <div className="collapse-content text-sm">
+                Click on "Forgot Password" on the login page and follow the
+                instructions sent to your email.
+              </div>
+            </div>
+            <div className="collapse collapse-plus bg-base-100 border border-base-300">
+              <input type="radio" name="my-accordion-2" />
+              <div className="collapse-title font-semibold flex justify-between">
+              <span>Inventory System</span>
+              <span>June-2022 - August-2022</span>
+            </div>
+              <div className="collapse-content text-sm">
+                Go to "My Account" settings and select "Edit Profile" to make
+                changes.
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
+export default Portfolio;
