273ac8e7dba01f16ab74aa4ae2682241901e883d
Aceofthehood1 <geoffreyameble81@gmail.com>
2025-10-13 23:36:39 +0100

    Replaced accordion with collaspe components and remodelled homepage and portfolio page to look more professional

--- portfolio/src/Pages/HomePage.tsx
+++ portfolio/src/Pages/HomePage.tsx
@@ -8,20 +8,21 @@ interface Props {}
 const HomePage = (props: Props) => {
   return (
     <>
-      <div className="flex flex-col items-center justify-center h-screen bg-amber-500">
+      <div className="flex flex-col items-center justify-center h-screen p-5">
         <div className="text-left max-w-[800px]">
           <h2 className="text-5xl mb-2">Hi, I'm</h2>
           <h1 className="text-5xl">GEOFFREY AMEBLE</h1>
           <hr className="border-2"></hr>
-          <h2 className="text-3xl mb-2">Web Developer and Software Engineer</h2>
+          <h2 className="text-3xl">Web Developer and Software Engineer</h2>
+          <h2 className="text-xl mb-2" >Turning complex ideas into functional, scalable, and user-friendly software.</h2>
           <div className="flex flex-row gap-2 text-lg">
-            <button className="bg-blue-700 border-2 border-black rounded-lg text-white p-2">
+            <button className="bg-red-600 border-2 border-black rounded-lg text-white p-2">
               <Link to="/portfolio">
                 <FontAwesomeIcon icon={faFileLines} />
                 Portfolio
               </Link>
             </button>
-            <button className="bg-blue-700 border-2 border-black rounded-lg text-white p-2">
+            <button className="bg-red-600 border-2 border-black rounded-lg text-white p-2">
               <a
                 href="https://github.com/Aceofthehood1"
                 target="_blank"
@@ -31,7 +32,7 @@ const HomePage = (props: Props) => {
                 Github
               </a>
             </button>
-            <button className="bg-blue-700 border-2 border-black rounded-lg text-white p-2">
+            <button className="bg-red-600 border-2 border-black rounded-lg text-white p-2">
               <a
                 href="https://www.linkedin.com/in/geoffrey-ameble/"
                 target="_blank"
--- portfolio/src/Pages/Portfolio.tsx
+++ portfolio/src/Pages/Portfolio.tsx
@@ -1,85 +1,108 @@
+import { faBriefcase, faLaptopCode } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 interface Props {}
 
 const Portfolio = (props: Props) => {
   return (
     <>
-      <div className="flex flex-col gap-8 sm:flex-row items-center justify-center mt-[100px]">
-        <div className=" flex flex-col gap-2 max-w-[600px]">
-          <h1 className="text-5xl mb-4">Experiences</h1>
-          <div className="collapse collapse-plus bg-base-100 border border-base-300">
-            <input type="radio" name="my-accordion-3" defaultChecked />
-            <div className="collapse-title font-semibold flex justify-between">
-              <span>SAP Investment Holdings</span>
-              <span>Feb-2024 - Current</span>
-            </div>
-            <div className="collapse-content text-sm">
-              Click the "Sign Up" button in the top right corner and follow the
-              registration process.
-            </div>
-          </div>
-          <div className="collapse collapse-plus bg-base-100 border border-base-300">
-            <input type="radio" name="my-accordion-3" />
-            <div className="collapse-title font-semibold flex justify-between">
-              <span>Kato</span>
-              <span>March-2025 - July-2025</span>
-            </div>
-            <div className="collapse-content text-sm">
-              Click on "Forgot Password" on the login page and follow the
-              instructions sent to your email.
-            </div>
-          </div>
-          <div className="collapse collapse-plus bg-base-100 border border-base-300">
-            <input type="radio" name="my-accordion-3" />
-            <div className="collapse-title font-semibold flex justify-between">
-              <span>Turntabl</span>
-              <span>June-2022 - August-2022</span>
-            </div>
-            <div className="collapse-content text-sm">
-              Go to "My Account" settings and select "Edit Profile" to make
-              changes.
-            </div>
-          </div>
-        </div>
-
-        <div className="flex flex-col max-w-[600px]">
-          <h1 className="text-5xl mb-4">Projects</h1>
-          <div className=" flex flex-col gap-2 max-w-[600px]">
-            <div className="collapse collapse-plus bg-base-100 border border-base-300">
-              <input type="radio" name="my-accordion-2" defaultChecked />
-              <div className="collapse-title font-semibold flex justify-between">
-              <span>Restaurant Reservation Marketplace</span>
-              <span>June-2022 - August-2022</span>
-            </div>
+      <section className=" h-screen pt-[100px]">
+        <div className="flex flex-col gap-8 sm:flex-row justify-center">
+          <div className=" flex flex-col gap-2 max-w-[500px] p-4 sm:min-w-[600px]">
+            <h1 className="text-5xl mb-4 text-red-600">
+              <FontAwesomeIcon icon={faBriefcase} />
+              Experiences
+            </h1>
+            <details className="collapse bg-base-100 border-base-300 border">
+              <summary className="collapse-title font-semibold">
+                <span className=" text-xl text-red-600">Web Developer / Tech Analyst</span>
+                <div className="flex justify-between">
+                  <span>SAP Investment Holdings</span>
+                  <span>Feb-2024 - Current</span>
+                </div>
+              </summary>
               <div className="collapse-content text-sm">
                 Click the "Sign Up" button in the top right corner and follow
                 the registration process.
               </div>
-            </div>
-            <div className="collapse collapse-plus bg-base-100 border border-base-300">
-              <input type="radio" name="my-accordion-2" />
-              <div className="collapse-title font-semibold flex justify-between">
-              <span>Vehicle Management System</span>
-              <span>June-2022 - August-2022</span>
-            </div>
+            </details>
+            <details className="collapse bg-base-100 border-base-300 border">
+              <summary className="collapse-title font-semibold">
+                <span className=" text-xl text-red-600">Web Developer / Tech Analyst</span>
+                <div className="flex justify-between">
+                  <span>SAP Investment Holdings</span>
+                  <span>Feb-2024 - Current</span>
+                </div>
+              </summary>
               <div className="collapse-content text-sm">
-                Click on "Forgot Password" on the login page and follow the
-                instructions sent to your email.
+                Click the "Sign Up" button in the top right corner and follow
+                the registration process.
               </div>
-            </div>
-            <div className="collapse collapse-plus bg-base-100 border border-base-300">
-              <input type="radio" name="my-accordion-2" />
-              <div className="collapse-title font-semibold flex justify-between">
-              <span>Inventory System</span>
-              <span>June-2022 - August-2022</span>
-            </div>
+            </details>
+            <details className="collapse bg-base-100 border-base-300 border">
+              <summary className="collapse-title font-semibold">
+                <span className=" text-xl text-red-600">Web Developer / Tech Analyst</span>
+                <div className="flex justify-between">
+                  <span>SAP Investment Holdings</span>
+                  <span>Feb-2024 - Current</span>
+                </div>
+              </summary>
               <div className="collapse-content text-sm">
-                Go to "My Account" settings and select "Edit Profile" to make
-                changes.
+                Click the "Sign Up" button in the top right corner and follow
+                the registration process.
               </div>
-            </div>
+            </details>
           </div>
+
+          <div className="flex flex-col gap-2 max-w-[500px] p-4 sm:min-w-[600px]">
+            <h1 className="text-5xl mb-4 text-red-600">
+              <FontAwesomeIcon icon={faLaptopCode} />
+              Projects
+            </h1>
+            <details className="collapse bg-base-100 border-base-300 border">
+              <summary className="collapse-title font-semibold">
+                <span className=" text-xl text-red-600">Web Developer / Tech Analyst</span>
+                <div className="flex justify-between">
+                  <span>SAP Investment Holdings</span>
+                  <span>Feb-2024 - Current</span>
+                </div>
+              </summary>
+              <div className="collapse-content text-sm">
+                Click the "Sign Up" button in the top right corner and follow
+                the registration process.
+              </div>
+            </details>
+            <details className="collapse bg-base-100 border-base-300 border">
+              <summary className="collapse-title font-semibold">
+                <span className=" text-xl text-red-600">Web Developer / Tech Analyst</span>
+                <div className="flex justify-between">
+                  <span>SAP Investment Holdings</span>
+                  <span>Feb-2024 - Current</span>
+                </div>
+              </summary>
+              <div className="collapse-content text-sm">
+                Click the "Sign Up" button in the top right corner and follow
+                the registration process.
+              </div>
+            </details>
+            <details className="collapse bg-base-100 border-base-300 border">
+              <summary className="collapse-title font-semibold">
+                <span className=" text-xl text-red-600">Web Developer / Tech Analyst</span>
+                <div className="flex justify-between">
+                  <span>SAP Investment Holdings</span>
+                  <span>Feb-2024 - Current</span>
+                </div>
+              </summary>
+              <div className="collapse-content text-sm">
+                Click the "Sign Up" button in the top right corner and follow
+                the registration process.
+              </div>
+            </details>
+          </div>
+
+          
         </div>
-      </div>
+      </section>
     </>
   );
 };
